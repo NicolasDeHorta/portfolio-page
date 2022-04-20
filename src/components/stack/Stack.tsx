2fcca8a4b8f412be538b0ce4ea6160faca0540d7
NicoDh <dehorta.n@gmail.com>
2022-04-19 22:20:25 -0300

    added name to stack icons

--- src/components/stack/Stack.tsx
+++ src/components/stack/Stack.tsx
@@ -26,8 +26,8 @@ const typescriptIcon: any = {
   prefix: "fac",
   iconName: "typescript",
   icon: [
-    20,
-    20,
+    23,
+    23,
     [],
     "",
     "M3,5v14c0,1.105,0.895,2,2,2h14c1.105,0,2-0.895,2-2V5c0-1.105-0.895-2-2-2H5C3.895,3,3,3.895,3,5z M13.666,12.451h-2.118	V19H9.841v-6.549H7.767V11h5.899V12.451z M13.998,18.626v-1.751c0,0,0.956,0.721,2.104,0.721c1.148,0,1.103-0.75,1.103-0.853	c0-1.089-3.251-1.089-3.251-3.501c0-3.281,4.737-1.986,4.737-1.986l-0.059,1.559c0,0-0.794-0.53-1.692-0.53	c-0.897,0-1.221,0.427-1.221,0.883c0,1.177,3.281,1.059,3.281,3.428C19,20.244,13.998,18.626,13.998,18.626z",
@@ -36,22 +36,46 @@ const typescriptIcon: any = {
 
 export const Stack = () => {
   const stackItems = [
-    faReact,
-    faJs,
-    faPython,
-    faNode,
-    faGit,
-    faDatabase,
-    faGrapQl,
-    typescriptIcon,
+    {
+      item: faReact,
+      name: "React",
+    },
+    {
+      item: faJs,
+      name: "JavaScript",
+    },
+    {
+      item: faPython,
+      name: "Python",
+    },
+    {
+      item: faNode,
+      name: "Node.js",
+    },
+    {
+      item: faGit,
+      name: "git",
+    },
+    {
+      item: faDatabase,
+      name: "SQL",
+    },
+    {
+      item: faGrapQl,
+      name: "GraphQL",
+    },
+    {
+      item: typescriptIcon,
+      name: "TypeScript",
+    },
   ];
 
   return (
     <div className="main">
       <div className="container">
-        {stackItems.map((item, index) => {
+        {stackItems.map(({ name, item }, index) => {
           return (
-            <div className={`item item${index}`}>
+            <div className={`item item${index}`} data-info={name}>
               <FontAwesomeIcon className={`stackItem`} icon={item} />
             </div>
           );
